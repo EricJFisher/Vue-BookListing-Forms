830f77beb292294950ffe9133e807346362fc164
Eric J Fisher <eric-fisher@pluralsight.com>
2018-10-09 14:54:26 -0400

    Update src/components/BookForm.vue

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -12,15 +12,17 @@ export default {
   props: ["books"],
   data() {
     return {
-      bookTitle: "",
-      bookAuthor: "",
-      finishedReading: false,
-      owenership: []
+      bookData: {
+        bookTitle: "",
+        bookAuthor: "",
+        finishedReading: false,
+        ownership: []
+      }
     };
   },
   methods: {
-    bookSubmit(bookTitle, bookAuthor) {
-      this.$emit("addBook", bookTitle, bookAuthor);
+    bookSubmit(bookData) {
+      this.$emit("addBook", bookData);
     }
   }
 };
